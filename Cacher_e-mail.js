c6781bf234449117d7f636deab8f794d73df6426
Alexis C <chaponalexis@gmail.com>
2019-06-03 14:31:14 +0200

    Ajout du troisième fichier Cacher_e-mail.js

---
+++ Cacher_e-mail.js
@@ -0,0 +1,9 @@
+
+function genadresse(utilisateur) {    
+    var domain = "votredomaine";           // indiquez ici le nom de votre domaine
+    var pays  ="fr";                    // fr, com, be, ch
+    var atsign = String.fromCharCode(64);  // c'est le signe @
+    var point  = String.fromCharCode(46);  // c'est le "." point
+    var adr =  utilisateur + atsign + domain + point + pays;
+    document.write("<" + "a" + " " + "href=" + "ma" +String.fromCharCode(105) + "lto" +String.fromCharCode(58) + adr +">" + adr + "");
+  }
